4d3ddb391d1de80b230b5d73ffa509513ae1aea8
FuadKhalit <ahmadfuad@protonmail.com>
2020-07-07 18:19:03 +0800

    Update the file with latest matplotlib to fix plotting issue. remove lines 2 as we are not using it at all

--- Apollo2 (TOP_GLOVE).py
+++ Apollo2 (TOP_GLOVE).py
@@ -13,50 +13,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 sia = SentimentIntensityAnalyzer()
 
 
-# In[ ]:
-
-
-import csv
-import pandas as pd
-
-# stock market lexicon
-stock_lex = pd.read_csv('lexicon_data/stock_lex.csv')
-stock_lex['sentiment'] = (stock_lex['Aff_Score'] + stock_lex['Neg_Score'])/2
-stock_lex = dict(zip(stock_lex.Item, stock_lex.sentiment))
-stock_lex = {k:v for k,v in stock_lex.items() if len(k.split(' ')) == 1}
-stock_lex_scaled = {}
-for k,v in stock_lex.items():
-    if v>0:
-        stock_lex_scaled[k] = v / max(stock_lex.values()) * 4
-    else:
-        stock_lex_scaled[k] = v / min(stock_lex.values()) * -4
-        
-# loughran and MacDonald
-positive = []
-with open('lexicon_data/lm_positive.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        positive.append(row[0].strip())
-        
-negative = []
-with open('lexicon_data/lm_negative.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        entry = row[0].strip().split(" ")
-        if len(entry) > 1:
-            negative.extend(entry)
-        else:
-            negative.extend(entry[0])
-            
-final_lex = {}
-final_lex.update({word:2.0 for word in positive})
-final_lex.update({word:-2.0 for word in negative})
-final_lex.update(stock_lex_scaled)
-final_lex.update(sia.lexicon)
-sia.lexicon = final_lex
-
-
-# In[5]:
+# In[3]:
 
 
 from urllib.request import urlopen
@@ -104,10 +61,10 @@ print(date_sentiment)
 # In[ ]:
 
 
-pip install backtrader
+pip install matplotlib
 
 
-# In[6]:
+# In[4]:
 
 
 from __future__ import (absolute_import, division, print_function,unicode_literals)
@@ -225,7 +182,7 @@ class SentimentStrat(bt.Strategy):
              (self.params.period, self.broker.getvalue()), doprint=True)
 
 
-# In[7]:
+# In[ ]:
 
 
 if __name__ == '__main__':
@@ -252,7 +209,7 @@ if __name__ == '__main__':
     cerebro.run()
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     
-    cerebro.plot()
+    cerebro.plot(iplot=False)
 
 
 # In[ ]:
